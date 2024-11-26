574a3906811aa569b936ad053b095657ef4c64e7
luis-belim <luis.blima14@gmail.com>
2024-11-26 14:20:03 -0300

    Add files via upload

---
+++ agente_gerador_post_linkedin.py
@@ -0,0 +1,8 @@
+
+# Substitua isso pelo seu código do agente
+class Agente:
+    def gerar_post(self, tema):
+        return f"✨ Post criado pelo agente sobre: '{tema}'"
+
+# Instância do agente
+agente = Agente()
